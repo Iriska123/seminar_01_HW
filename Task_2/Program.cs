c1a7d8da5ccb535db014c0a0d9653b7f8797ec2a
Iriska123 <irinapetr1110@mail.ru>
2023-07-20 21:55:19 +0300

    add Task 01

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b) {
+    Console.WriteLine($"MAX = {a} MIN = {b}");
+} else {
+    Console.WriteLine($"MAX = {b} MIN = {a}");
+}

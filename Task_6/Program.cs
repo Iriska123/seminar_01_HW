814cbf5c9b030ceba81f28862ea2d93133385bb0
Iriska123 <irinapetr1110@mail.ru>
2023-07-20 22:49:59 +0300

    add Task 06

---
+++ Task_6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Int32.Parse(Console.ReadLine());
+
+if(a % 2 == 0) {
+    Console.WriteLine(a + " - четное число");
+} else {
+    Console.WriteLine(a + " - нечетное число");
+}
